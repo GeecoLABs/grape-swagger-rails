d2f6b66c087491f326c48bd8117d8e85478d4ad6
Grzegorz Brzezinka <grzegorz@brzezinka.eu>
2015-06-24 01:13:11 +0200

    multiple OAuth2 and token-related changes: - support OAuth2 init, - pass initial api_key, - properly add keys on init and on field change

--- lib/grape-swagger-rails.rb
+++ lib/grape-swagger-rails.rb
@@ -24,6 +24,7 @@ module GrapeSwaggerRails
     api_auth:             '',        # 'basic' or 'bearer'
     api_key_name:         'api_key', # 'Authorization'
     api_key_type:         'query',   # 'header'
+    api_key_initial:      '',
 
     oauth2:               {},
 
